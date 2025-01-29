9fb4c686291df73696395ec5f2a748f58be02c92
Sam <144938568+SamLo2200@users.noreply.github.com>
2025-01-29 19:33:27 +0800

    Added emoji expression to increase readability

--- Compress_On_Save.sh
+++ Compress_On_Save.sh
@@ -12,21 +12,22 @@ fi
 
 WORKING_DIR="$1"
 
-echo "Working directory: $WORKING_DIR"
+echo "🗂️ Working directory: $WORKING_DIR"
 
 SOURCE_DIR="$WORKING_DIR/mya_server_custom_models_resource_pack"
 MINECRAFT_PATH="$HOME/Library/Application Support/minecraft/resourcepacks"
 ARCHIVE_NAME="../Mya_Server_Custom_Models_Resource_Pack_1.21.4.zip"
 
 if [ -d "$MINECRAFT_PATH" ]; then
-    echo "Minecraft is installed at: $MINECRAFT_PATH"
+    echo "📍 Minecraft is installed at: $MINECRAFT_PATH"
     cd "$SOURCE_DIR"
     rm "$ARCHIVE_NAME"
-    echo "Removed $ARCHIVE_NAME"
+    echo "🗑️ Removed $ARCHIVE_NAME"
+    echo "📦 Zipping the resource pack..."
     zip -r "$ARCHIVE_NAME" ./* -x "*.DS_Store"
     cp "$ARCHIVE_NAME" "$MINECRAFT_PATH"
-    echo "Successfully zipped and copied the resource pack."
+    echo "🎉 Successfully zipped and copied the resource pack."
 else
-    echo "Minecraft is not installed in the default location. Failed to obtain the path."
+    echo "😔 Minecraft is not installed in the default location. Failed to obtain the path."
 fi
 
